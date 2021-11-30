9a9d2e846be0e22291b45d78cb6ee15ae1ca7ab9
siebecommeene <siebe.commeene@ugent.be>
2021-11-30 16:19:18 +0100

    change logo

--- components/NavigationMobile.js
+++ components/NavigationMobile.js
@@ -34,8 +34,8 @@ const NavigationMobile = ({ locale, locales }) => {
         <div className={styles.navlogo}>
           <a href="/">
             <img
-              src="https://a.storyblok.com/f/133261/3039x582/a60d166ec2/logo-colored-full.png/m/200x0"
-              alt="IMDBPlus Logo"
+              src="https://a.storyblok.com/f/134952/500x500/ebe8675a16/themusicpro-2_adobespark.png"
+              alt="TheMusicPro Logo"
               className=""
             />
           </a>
