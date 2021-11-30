31b5163ac57f4ecfb58e6f5d4cc99980d875b119
siebecommeene <siebe.commeene@ugent.be>
2021-11-30 16:00:34 +0100

    change logo

--- components/Navigation.js
+++ components/Navigation.js
@@ -30,8 +30,8 @@ const Navigation = ({ locale, locales }) => {
         <div className={styles.navlogo}>
           <a href={homeurl}>
           {/* <img
-              src="https://a.storyblok.com/f/133261/3039x582/a60d166ec2/logo-colored-full.png/m/200x0"
-              alt="IMDBPlus Logo"
+              src="https://a.storyblok.com/f/134952/500x500/ebe8675a16/themusicpro-2_adobespark.png"
+              alt="TheMusicPro Logo"
               className=""
             /> */}
             <Image src="/THEMUSICPRO.png" alt="IMDBPlus Logo" width="128" height="25" />
