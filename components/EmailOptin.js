6c37026ae57595ab175909d3156b3579f8bdd581
siebecommeene <siebe.commeene@ugent.be>
2021-12-16 13:39:54 +0100

    added emailoptin

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+      {formstate&&<iframe width="540" height="900" src="https://736f53c4.sibforms.com/serve/MUIEAA7-TZyrro5Cmo7idXkKDm7R7r1aMDA-Ih8WsTdPfPW-xO_qbGretmH-xsoXDMcHMT4wxVbkcE9uvVoIGnJfoVfZcpkPPh2FrYej36_R5h2QxDW3qRJuMcsRiPHKDNyvuS2cLDznu8h2mQTiGWYze3XnWxK7Qu7vKdnDpdUUBLlA5oJXegty8gCotLh7qoOgoRDZBYelLZW9" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} 
     </div>
   );
 };
