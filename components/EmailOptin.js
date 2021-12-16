81816ce12472af146ab0d1571f31b7ec8e8140ff
siebecommeene <siebe.commeene@ugent.be>
2021-12-16 13:56:28 +0100

    change email

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {formstate&&<iframe width="540" height="900" src="https://736f53c4.sibforms.com/serve/MUIEAJEzSn8mfNf47ujqS0JPokK-tvNXCXGx2FuzENIOVbxj0y4ecS0s-tTDDuAp-32MPlm333yAi3Vb_B37AMw6e4BaqMS6cbu8-PGdsU-6f7wR8lzq80XNLxNMO424MjLSsRAWDpi64BDhvT1a-sd6PGOzAMdCWXieNdNIG-pxH6b7Ho9ilAAKOSqEiCM0Fx_0BunysT6mNmjw" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} 
+      {formstate&&<iframe width="540" height="900" src="https://736f53c4.sibforms.com/serve/MUIEAMwbXOdlJ5yx9ix2QRTC7X4dxJBT2QMJlsKOsA9brGvEHA0uVfg5tFAx4WlqZVJHenAf8nZLsUkYwjSKe2QhEzRA2yjCJMjF5pICLxAXARVixPfBiaQMckJg_JylGwm9rJ-ntGVAo09Px4u9ogK4ablRitykfGWYFo9VXpn_qePVpzlZ_An3StUG2vZplbHsy0q2zpdMVae_" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} 
     </div>
   );
 };
